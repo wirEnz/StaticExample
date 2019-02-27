04749b0288f0279387fed48871dc066290389995
wirEnz <fps.andrew@gmail.com>
2019-02-27 20:37:51 +1300

    StaticExample

---
+++ src/Account.java
@@ -0,0 +1,15 @@
+
+public class Account {
+	
+	public Account(String string, int i, double d) {
+		
+	} 
+		// TODO Auto-generated constructor stub
+	
+	public String personName;		// Name
+	public int accountNumber;		// Number
+	public static double fee;		// Monthly account fee
+	public double balance;		// Current balance
+}
+
+
---
+++ src/StaticExample.java
@@ -0,0 +1,29 @@
+
+public class StaticExample {
+	
+	public static void main(String[] args) {
+		
+		
+		// Create account object for John
+		Account johnAccount = new Account("John",1020473,1589.39);
+		// Create account object for Mary
+		Account maryAccount = new Account("Mary",1020473,1590.39);
+		
+		// Set the account fee
+		Account.fee = 12.40;
+		
+		// Display account fees
+		System.out.println("John's fee: " + johnAccount.fee);
+		System.out.println("Mary's fee: " + maryAccount.fee);
+		
+		// Change account fee
+		Account.fee = 6.20;
+		
+		// Display account fees again
+		System.out.println("John's fee: " + johnAccount.fee);
+		System.out.println("Mary's fee: " + maryAccount.fee);
+	}
+}
+
+	
+			 
